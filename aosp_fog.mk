a64333706266e0de88c491b266e258577f2e7ce0
alternoegraha <noegrahachan@gmail.com>
2025-02-02 21:03:56 +0700

    fog: don't build ARCore and Live Wallpapers - debloat a bit
    Signed-off-by: Reimu <192520540+Keera00@users.noreply.github.com>

--- aosp_fog.mk
+++ aosp_fog.mk
@@ -10,9 +10,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 720
+
+# PixelOS Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+
+# Animation Quality
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
